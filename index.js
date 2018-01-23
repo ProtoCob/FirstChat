16ec5a97c1d72dab108eaf649c3f5f0ec3162cd0
ProtoCob <35745233+ProtoCob@users.noreply.github.com>
2018-01-23 22:15:28 +0100

    draft

---
+++ index.js
@@ -0,0 +1,41 @@
+// ProtoBot webhooks
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.set('port', (process.env.port ¦¦ 5000))
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+// Spin up the server
+app.listen(app.get('port'), function () {
+  console.log('running on port', app.get('port'))
+})
+
+// Index route
+app.get('/', function (req, res) {
+  res.send('Hello world, I am ProtoBot webhook.')
+})
+
+app.post('/booktable/', function (req, res) {
+  console.log(JSON.stringify(req.body));
+  // YOUR BOOKING CODE GOES HERE !!
+
+var out = {
+  "messages": [
+    {"text": "Thanks for your booking!"},
+    {"text": "See you soon!!"}
+  ]
+}
+
+var outString = JSON.stringify(out);
+console.log('Out:' + outString);
+
+res.send(outString);
+})
+
