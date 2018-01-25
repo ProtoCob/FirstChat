3ffdf537efce6af1742bbe215f1b7afa2dcc6ddb
ProtoCob <35745233+ProtoCob@users.noreply.github.com>
2018-01-25 23:11:02 +0100

    bugfix

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const bodyParser = require('body-parser')
 const app = express()
 //hard set port
 //app.set('port', (process.env.PORT || 5000))
-app.listen(process.env.PORT || 3000, function () {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
@@ -17,9 +14,12 @@ app.use(bodyParser.json())
 app.use(express.static('public'))
 
 // Spin up the server
-app.listen(app.get('port'), function () {
-  console.log('running on port', app.get('port'))
-})
+//app.listen(app.get('port'), function () {
+//  console.log('running on port', app.get('port'))
+//})
+app.listen(process.env.PORT || 5000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 // Index route
 app.get('/', function (req, res) {
