22ccdae1a18f9bba3e28ed883247ade3f9e91ba7
ProtoCob <35745233+ProtoCob@users.noreply.github.com>
2018-01-25 22:55:11 +0100

    bugfix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-app.set('port', (process.env.port || 5000))
+app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
